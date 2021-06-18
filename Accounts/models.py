2fa3d23578d9520bb720ba5f3fc9d34fda87be1e
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-06-18 20:59:03 +0100

    fixes

--- Accounts/models.py
+++ Accounts/models.py
@@ -8,15 +8,15 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=50, unique=True)
-    password = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    occupation = models.CharField(max_length=50, null=True)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    occupation = models.CharField(max_length=255, null=True)
     bio = models.TextField(null=True)
     verified = models.BooleanField(default=False)
-    status = models.CharField(max_length=50)
+    status = models.CharField(max_length=255)
     followers_count = models.IntegerField
     follows_count = models.IntegerField
     created_at = models.DateTimeField(auto_now_add=True)
@@ -30,11 +30,11 @@ class User(AbstractUser):
 class UserSocial(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-    facebook_link = models.CharField(max_length=100)
-    instagram_link = models.CharField(max_length=100)
-    twitter_link = models.CharField(max_length=100)
-    linkedin_link = models.CharField(max_length=100)
-    github_link = models.CharField(max_length=100)
+    facebook_link = models.CharField(max_length=255)
+    instagram_link = models.CharField(max_length=255)
+    twitter_link = models.CharField(max_length=255)
+    linkedin_link = models.CharField(max_length=255)
+    github_link = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
