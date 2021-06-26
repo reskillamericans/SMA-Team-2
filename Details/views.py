5c33b5a81ce3b5e347e6c709cd6edefe0bb104d3
Marco Apraez <maz_2002@email.com>
2021-06-25 23:54:16 -0700

    fixes and cleanup

--- Details/views.py
+++ Details/views.py
@@ -8,20 +8,18 @@ from .models import Post, PostCategory
 def create_post(request):
     if not request.user.is_authenticated:
         return redirect("index")
-    if request.method == 'POST':
-        if not request.POST.get('title') and request.POST.get('content'):
-            return redirect("index")
-        post = Post()
-        post.user_id = request.user
-        post_category_id = get_category_id(request.POST.get('category'))
-        if post_category_id is not None:
-            post.post_category_id = post_category_id
-        post.title = request.POST.get('title')
-        post.content = request.POST.get('content')
-        post.save()
-        return redirect("index")
-    else:
+    if request.method != 'POST':
         return render(request=request, template_name="create_post.html")
+    if not request.POST.get('title') and request.POST.get('content'):
+        return redirect("index")
+    post = Post()
+    post.user_id = request.user
+    post.post_category_id = get_category_id(request.POST.get('category'))
+    post.title = request.POST.get('title')
+    post.content = request.POST.get('content')
+    post.save()
+    return redirect("index")
+
 
 
 def get_category_id(category):
@@ -33,9 +31,9 @@ def get_category_id(category):
         return post_category
     except PostCategory.DoesNotExist:
         # Else Create category
-        category = PostCategory(name=category)
-        category.save()
-        return category
+        new_category = PostCategory(name=category)
+        new_category.save()
+        return new_category
 
 
 def delete_post(request, id):
@@ -50,16 +48,18 @@ def delete_post(request, id):
 def update_post(request, id):
     if not request.user.is_authenticated:
         return redirect("index")
-    post = Post.objects.get(id=id)
+
+    try:
+        post = Post.objects.get(id=id)
+    except Post.DoesNotExist:
+        return redirect('index')
+
     if not request.user == post.user_id:
         return redirect('index')
-    if request.method == 'UPDATE':
-        post_category_id = get_category_id(request.POST.get('category'))
-        if post_category_id is not None:
-            post.post_category_id = post_category_id
-        post.title = request.POST.get('title')
-        post.content = request.POST.get('content')
-        post.save()
-        return redirect('index')
-    else:
+    if request.method != 'POST':
         return render(request=request, template_name="update_post.html")
+    post.post_category_id = get_category_id(request.POST.get('category'))
+    post.title = request.POST.get('title')
+    post.content = request.POST.get('content')
+    post.save()
+    return redirect('index')
