f5e94fb9c5577390877586af8b3144a100dc5f2b
Pia Torain <piatorain@yahoo.com>
2021-07-29 15:42:11 -0700

    First commit of the registration Page

---
+++ Register/registration.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const register = document.getElementById("register");
+    const confirmAccount = document.getElementById("accountConfirmed");
+    const form = document.getElementById("registration-form");
+    const userName = document.getElementById("user_name");
+    const email = document.getElementById("user_email");
+    const pwd = document.getElementById("user_password");
+    const BtnSignup = document.getElementById("sign-up");
+
+
+    confirmAccount.style.display ="none";
+
+
+    form.addEventListener('input', function(e) {
+        BtnSignup.addEventListener('click', function(e) {
+            e.preventDefault();
+        }); 
+        if(userName.value !== "" && email.validity.valid && pwd.validity.valid) {
+            BtnSignup.style.backgroundColor = "#9E8BC7";
+            BtnSignup.addEventListener('click', showConfirmed);  
+        } else{ 
+            BtnSignup.style.backgroundColor = "#CCCCCC";
+        };
+    });
+
+    function showConfirmed(){
+        register.style.display = "none";
+        confirmAccount.style.display = "block";
+    };
+    
+
+    
+    
+});    
